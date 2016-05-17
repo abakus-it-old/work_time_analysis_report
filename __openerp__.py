8cf11b8905ae83ae706b96c6f043601eae9e991f
Valentin Thirion <valentinthirion@gmail.com>
2016-05-17 12:01:51 +0200

    Added description

--- __openerp__.py
+++ __openerp__.py
@@ -4,7 +4,9 @@
     'category': 'Human Resources',
     'description': 
     """
-    Work time analysis report. 
+    Work time analysis report.
+
+    This modules adds a report, accessible by a wizard, that allows to print a timesheet report for teams on contracts.
     
     This module has been developed by Bernard Delhez, intern @ AbAKUS it-solutions.
     """,
